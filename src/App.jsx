36a75e0728750d17e7f607a685b6b194df797181
korkouanastasia4-lgtm <korkouanastasia4@gmail.com>
2026-06-02 21:55:58 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
-    <h1 style={{ padding: 20 }}>
-      ALC Calendar WORKS ✅
-    </h1>
+    <div style={{ padding: 20, fontFamily: 'Arial' }}>
+      <h1>ALC Calendar is LIVE ✅</h1>
+    </div>
   )
 }
