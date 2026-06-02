c79047c1d4cb68e7dfd1558b4c7a7aa12cbf9d75
korkouanastasia4-lgtm <korkouanastasia4@gmail.com>
2026-06-02 22:39:39 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,10 @@
-import logo from "./logo.png";
+import logo from "../logo.png";
 
 export default function App() {
   return (
-    <div style={styles.container}>
-      <img src={logo} alt="ALC Logo" style={styles.logo} />
-
-      <h1 style={styles.title}>ALC Calendar is LIVE ✅</h1>
+    <div style={{ textAlign: "center", marginTop: 100 }}>
+      <img src={logo} width={150} />
+      <h1>ALC Calendar is LIVE ✅</h1>
     </div>
   );
 }
-
-const styles = {
-  container: {
-    height: "100vh",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#ffffff",
-  },
-  logo: {
-    width: 140,
-    marginBottom: 20,
-  },
-  title: {
-    fontSize: 28,
-    fontWeight: "bold",
-    color: "#222",
-  },
-};
