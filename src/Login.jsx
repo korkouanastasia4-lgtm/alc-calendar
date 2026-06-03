e56b3e621525eaadf4eb3e4ae0e4c470315277f8
korkouanastasia4-lgtm <korkouanastasia4@gmail.com>
2026-06-03 15:58:26 +0300

    Update Login.jsx

--- src/Login.jsx
+++ src/Login.jsx
@@ -4,60 +4,44 @@ import { supabase } from "./lib/supabase";
 export default function Login({ onLogin }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [mode, setMode] = useState("login");
 
-  const handleAuth = async () => {
-    if (mode === "login") {
-      const { data, error } = await supabase.auth.signInWithPassword({
-        email,
-        password,
-      });
+  const handleLogin = async () => {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
 
-      if (error) return alert(error.message);
-
+    if (!error) {
       onLogin(data.user);
     } else {
-      const { data, error } = await supabase.auth.signUp({
-        email,
-        password,
-      });
-
-      if (error) return alert(error.message);
-
-      alert("Account created! Now login.");
-      setMode("login");
+      alert(error.message);
     }
   };
 
   return (
-    <div style={{ textAlign: "center", marginTop: 100 }}>
-      <h2>{mode === "login" ? "Login" : "Register"}</h2>
+    <div>
+      <h3>Login</h3>
 
       <input
-        placeholder="Email"
+        placeholder="email"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
-        style={{ display: "block", margin: "10px auto", padding: 10 }}
       />
+
+      <br />
 
       <input
         type="password"
-        placeholder="Password"
+        placeholder="password"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
-        style={{ display: "block", margin: "10px auto", padding: 10 }}
       />
 
-      <button onClick={handleAuth} style={{ padding: 10 }}>
-        {mode === "login" ? "Login" : "Register"}
-      </button>
+      <br /><br />
 
-      <p
-        style={{ cursor: "pointer", marginTop: 20 }}
-        onClick={() => setMode(mode === "login" ? "register" : "login")}
-      >
-        Switch mode
-      </p>
+      <button onClick={handleLogin}>
+        Login
+      </button>
     </div>
   );
 }
